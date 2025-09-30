13d5f1c1e641f618e6e5fdb1d431e06aa03f74ad
Ahnaf Ahtasyafi <147116330+ahnafyura@users.noreply.github.com>
2025-09-30 15:08:15 +0700

    Update kpp_divisi_programming.cpp

--- kpp_divisi_programming.cpp
+++ kpp_divisi_programming.cpp
@@ -1,74 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX_ENERGY = 1000;
+const int energiMaks = 1000;
 
-struct Edge {
-    int to;
-    int length;
-    int obstacle;
-};
-
-struct State {
-    int node;
-    int minuteMod60;
-    int hourParity;
-    int consumed;
-    bool operator==(const State &o) const {
-        return node == o.node && minuteMod60 == o.minuteMod60 &&
-               hourParity == o.hourParity && consumed == o.consumed;
-    }
-};
-
-struct StateHash {
-    size_t operator()(const State &s) const noexcept {
-        return ((size_t)s.node * 1000003u) ^ ((size_t)s.minuteMod60 * 1009u) ^
-               ((size_t)s.hourParity * 9176u) ^ ((size_t)s.consumed * 1315423911u);
-    }
-};
-
-struct ParentInfo {
-    State prev;
-    int prevNode;
-    int arrivalMinute;
+class Edge {
+public:
+    int tujuan;
+    int panjang;
+    int halangan;
+    Edge(int t, int l, int o) : tujuan(t), panjang(l), halangan(o) {}
 };
 
 class Graph {
 public:
     int N;
-    unordered_map<string, int> nameToId;
-    vector<string> idToName;
+    unordered_map<string,int> NamakeID;
+    vector<string> IDkenama;
     vector<vector<Edge>> adj;
-    vector<char> isRestPoint, isCharge;
+    vector<char> isRest, isCharge;
 
-    Graph(int n = 0) { init(n); }
+    Graph(int n=0){ init(n); }
 
-    void init(int n) {
-        N = n;
-        adj.assign(N + 1, {});
-        isRestPoint.assign(N + 1, 0);
-        isCharge.assign(N + 1, 0);
-        idToName.assign(N + 1, "");
+    void init(int n){
+        N=n;
+        adj.assign(N+1,{});
+        isRest.assign(N+1,0);
+        isCharge.assign(N+1,0);
+        IDkenama.assign(N+1,"");
     }
 
-    int getId(const string &name) {
-        if (nameToId.count(name)) return nameToId[name];
-        int id = (int)nameToId.size() + 1;
-        nameToId[name] = id;
-        if ((int)idToName.size() < id + 1) idToName.resize(id + 1);
-        idToName[id] = name;
+    int getId(const string &s){
+        if(NamakeID.count(s)) return NamakeID[s];
+        int id=(int)NamakeID.size()+1;
+        NamakeID[s]=id;
+        if((int)IDkenama.size()<=id) IDkenama.resize(id+1);
+        IDkenama[id]=s;
         return id;
     }
 
-    void addEdge(const string &u, const string &v, int w, int o) {
-        int a = getId(u), b = getId(v);
-        if ((int)adj.size() <= max(a, b)) {
-            adj.resize(max(a, b) + 1);
-            isRestPoint.resize(max(a, b) + 1);
-            isCharge.resize(max(a, b) + 1);
+    void addEdge(const string &u,const string &v,int w,int o){
+        int a=getId(u), b=getId(v);
+        if((int)adj.size()<=max(a,b)){
+            adj.resize(max(a,b)+1);
+            isRest.resize(max(a,b)+1);
+            isCharge.resize(max(a,b)+1);
         }
-        adj[a].push_back({b, w, o});
-        adj[b].push_back({a, w, o});
+        adj[a].push_back(Edge(b,w,o));
+        adj[b].push_back(Edge(a,w,o));
     }
 };
 
@@ -78,125 +56,89 @@ public:
     int startHour;
     int idStart, idTarget;
 
-    RobotNavigator(Graph &g, int hour) : graph(g), startHour(hour) {}
+    RobotNavigator(Graph &g,int h):graph(g),startHour(h){}
 
-    bool shortestPath(const string &start, const string &target) {
-        idStart = graph.getId(start);
-        idTarget = graph.getId(target);
+    bool shortestPath(const string &S,const string &T){
+        idStart=graph.getId(S);
+        idTarget=graph.getId(T);
 
-        using Item = tuple<int, int, State>; 
-        struct Cmp {
-            bool operator()(const Item &a, const Item &b) const {
-                if (get<0>(a) != get<0>(b)) return get<0>(a) > get<0>(b);
-                return get<1>(a) > get<1>(b);
-            }
-        };
+        using P=pair<int,int>;
+        vector<int> dist(graph.N+5,INT_MAX);
+        vector<int> energy(graph.N+5,energiMaks);
+        vector<int> parent(graph.N+5,-1);
+        vector<int> timeArr(graph.N+5,-1);
 
-        priority_queue<Item, vector<Item>, Cmp> pq;
-        unordered_map<State, int, StateHash> best;
-        unordered_map<State, ParentInfo, StateHash> parent;
+        priority_queue<P,vector<P>,greater<P>> pq;
+        dist[idStart]=0;
+        timeArr[idStart]=0;
+        pq.push({0,idStart});
 
-        State startState{idStart, 0, startHour % 2, 0};
-        best[startState] = 0;
-        parent[startState] = ParentInfo{startState, -1, 0};
-        pq.push({0, 0, startState});
+        while(!pq.empty()){
+            auto [d,u]=pq.top(); pq.pop();
+            if(d>dist[u]) continue;
 
-        bool found = false;
-        State goalState;
-
-        while (!pq.empty()) {
-            auto [totalEnergy, totalMinutes, st] = pq.top(); pq.pop();
-            if (best.find(st) != best.end() && best[st] < totalEnergy) continue;
-
-            if (st.node == idTarget) {
-                found = true;
-                goalState = st;
-                break;
+            if(u==idTarget){
+                printResult(dist,parent,timeArr);
+                return true;
             }
 
-            // recharge
-            if (graph.isCharge[st.node] && st.consumed != 0) {
-                State ns = st; ns.consumed = 0;
-                if (!best.count(ns) || best[ns] > totalEnergy) {
-                    best[ns] = totalEnergy;
-                    parent[ns] = {st, st.node, totalMinutes};
-                    pq.push({totalEnergy, totalMinutes, ns});
+            if(graph.isRest[u]){
+                int newTime=timeArr[u]+60;
+                int newCost=dist[u];
+                if(newCost<dist[u]){}
+            }
+
+            for(auto &e:graph.adj[u]){
+                int base=e.panjang+e.halangan;
+                int nowHour=(startHour*60+timeArr[u])/60;
+                double factor=(nowHour%2==0)?0.8:1.3;
+                int cost=(int)ceil(base*factor);
+
+                if(cost>energiMaks) continue;
+                if(cost>energy[u]) continue;
+
+                int newDist=dist[u]+cost;
+                int v=e.tujuan;
+                int newTime=timeArr[u]+2;
+
+                if(graph.isCharge[v]){
+                    energy[v]=energiMaks;
+                } else {
+                    energy[v]=energy[u]-cost;
                 }
-            }
 
-            if (graph.isRestPoint[st.node]) {
-                int toNextHour = (60 - st.minuteMod60) % 60;
-                if (toNextHour == 0) toNextHour = 60;
-                int newMinutes = totalMinutes + toNextHour;
-                int newMod60 = (st.minuteMod60 + toNextHour) % 60;
-                int newParity = 1 - st.hourParity;
-                State ns{st.node, newMod60, newParity, st.consumed};
-                if (!best.count(ns) || best[ns] > totalEnergy) {
-                    best[ns] = totalEnergy;
-                    parent[ns] = {st, st.node, totalMinutes}; 
-                    pq.push({totalEnergy, newMinutes, ns});
-                }
-            }
-
-            for (auto &e : graph.adj[st.node]) {
-                int baseLen = e.length + e.obstacle;
-                int energyCost = baseLen;
-
-                if (st.consumed + energyCost > MAX_ENERGY) continue;
-
-                int timeCost = 2;
-                int newMinutes = totalMinutes + timeCost;
-
-                int absoluteMinutes = startHour * 60 + newMinutes;
-                int newParity = (absoluteMinutes / 60) % 2;
-                int newMod60 = newMinutes % 60;
-
-                int newConsumed = st.consumed + energyCost;
-                if (graph.isCharge[e.to]) newConsumed = 0;
-
-                int newTotalEnergy = totalEnergy + energyCost;
-                State ns{e.to, newMod60, newParity, newConsumed};
-                if (!best.count(ns) || best[ns] > newTotalEnergy) {
-                    best[ns] = newTotalEnergy;
-                    parent[ns] = {st, st.node, newMinutes};
-                    pq.push({newTotalEnergy, newMinutes, ns});
+                if(newDist<dist[v]){
+                    dist[v]=newDist;
+                    parent[v]=u;
+                    timeArr[v]=newTime;
+                    pq.push({dist[v],v});
                 }
             }
         }
+        cout<<"Robot gagal dalam mencapai tujuan :(\n";
+        return false;
+    }
 
-        if (!found) {
-            cout << "Robot gagal dalam mencapai tujuan :(\n";
-            return false;
-        }
+    void printResult(vector<int>&dist,vector<int>&parent,vector<int>&timeArr){
+        cout<<"Total energi minimum: "<<dist[idTarget]<<"\n";
+        vector<int> path;
+        int cur=idTarget;
+        while(cur!=-1){ path.push_back(cur); cur=parent[cur]; }
+        reverse(path.begin(),path.end());
 
-        // reconstruct path
-        vector<pair<int,int>> path;
-        State cur = goalState;
-        while (true) {
-            ParentInfo pi = parent[cur];
-            path.push_back({cur.node, pi.arrivalMinute});
-            if (pi.prevNode == -1) break;
-            cur = pi.prev;
+        cout<<"Jalur: ";
+        for(size_t i=0;i<path.size();i++){
+            if(i) cout<<" -> ";
+            cout<<graph.IDkenama[path[i]];
         }
-        reverse(path.begin(), path.end());
-
-        cout << "Total minimum energy: " << best[goalState] << "\n";
-        cout << "Path: ";
-        for (size_t i = 0; i < path.size(); i++) {
-            if (i) cout << " -> ";
-            cout << graph.idToName[path[i].first];
+        cout<<"\nWaktu tiba:\n";
+        for(size_t i=0;i<path.size();i++){
+            cout<<graph.IDkenama[path[i]]<<" (menit "<<timeArr[path[i]]<<")\n";
         }
-        cout << "\n";
-        cout << "Arrival times:\n";
-        for (auto &p : path) {
-            cout << graph.idToName[p.first] << " (minute " << p.second << ")\n";
-        }
-        return true;
     }
 };
 
 int main() {
-    cout << "N x M: ";
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -204,12 +146,12 @@ int main() {
     cin >> N >> M;
     cout << "======PLAY=====" << endl;
 
-    Graph graph(N);
+    Graph g(N);
     for (int i = 0; i < M; i++) {
         string u, v;
         int w, o;
         cin >> u >> v >> w >> o;
-        graph.addEdge(u, v, w, o);
+        g.addEdge(u, v, w, o);
     }
 
     string S, T;
@@ -222,15 +164,18 @@ int main() {
         stringstream ss(line);
         string s;
         while (ss >> s) {
-            if (s == "-") { marker.assign(marker.size(), 0); return; }
-            int id = graph.getId(s);
+            if (s == "-") {
+                marker.assign(marker.size(), 0);
+                return;
+            }
+            int id = g.getId(s);
             if ((int)marker.size() <= id) marker.resize(id + 1);
             marker[id] = 1;
         }
     };
 
-    readList(graph.isRestPoint);
-    readList(graph.isCharge);
+    readList(g.isRest);
+    readList(g.isCharge);
     vector<char> dummy;
     readList(dummy);
     readList(dummy);
@@ -238,8 +183,9 @@ int main() {
     int startHour;
     cin >> startHour;
 
-    RobotNavigator robot(graph, startHour);
+    RobotNavigator robot(g, startHour);
     robot.shortestPath(S, T);
 
     return 0;
 }
+
